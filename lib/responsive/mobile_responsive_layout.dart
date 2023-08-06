56ed1df20250d4661fa52a8a99e894b851df097d
Vivu_ <vivek.21scse1010913@galgotiasuniversity.edu.in>
2023-08-06 23:33:07 +0530

    Day_2

--- lib/responsive/mobile_responsive_layout.dart
+++ lib/responsive/mobile_responsive_layout.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: file_names, unused_import
 
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
