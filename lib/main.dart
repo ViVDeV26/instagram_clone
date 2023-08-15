4853eeadfae906a147b0bb9ff630a7290010e4bb
Vivu_ <vivek.21scse1010913@galgotiasuniversity.edu.in>
2023-08-15 22:49:09 +0530

    Day 4

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:instagram_clone/responsive/mobile_responsive_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout.dart';
 import 'package:instagram_clone/responsive/web_responsive_layout.dart';
 import 'package:instagram_clone/screens/login_screen.dart';
+import 'package:instagram_clone/screens/signup_screen.dart';
 
 import 'package:instagram_clone/utils/colors.dart';
 
@@ -41,7 +42,7 @@ class MyApp extends StatelessWidget {
       // home: const ResponsiveLayout(
       //     webScreenLayout: WebScreenLayout(),
       //     mobileScreenLayout: MobileScreenLayout()),
-      home: const LoginScreen(),
+      home: const SignupScreen(),
     );
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,27 +1,30 @@
-// ignore_for_file: sort_child_properties_last, unused_import
+// ignore_for_file: sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:instagram_clone/screens/signup_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/widgets/text_field_input.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
   }
 
   @override
@@ -46,6 +49,35 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(
                 height: 64,
               ),
+              //icon for photo
+              Stack(
+                children: [
+                  const CircleAvatar(
+                    radius: 64,
+                    backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1687360440886-f220f137a16c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'),
+                  ),
+                  Positioned(
+                      bottom: -10,
+                      left: 80,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.add_a_photo,
+                          )))
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              //text input for username
+              TextFieldInput(
+                  textEditingController: _usernameController,
+                  hintText: 'Enter your username',
+                  textInputType: TextInputType.text),
+              const SizedBox(
+                height: 24,
+              ),
               //text input for email
               TextFieldInput(
                   textEditingController: _emailController,
@@ -60,6 +92,18 @@ class _LoginScreenState extends State<LoginScreen> {
                 hintText: 'Enter your password',
                 textInputType: TextInputType.text,
                 isPass: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+
+              //bio
+              TextFieldInput(
+                  textEditingController: _bioController,
+                  hintText: 'Enter your bio',
+                  textInputType: TextInputType.text),
+              const SizedBox(
+                height: 24,
               ),
               const SizedBox(
                 height: 24,
