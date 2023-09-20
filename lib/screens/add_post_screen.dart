0756f81814cd585d6952de015782539445773f1b
Vivu_ <vivek.21scse1010913@galgotiasuniversity.edu.in>
2023-09-20 15:18:52 +0530

    Day 8

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -10,10 +13,72 @@ class AddPostScreen extends StatefulWidget {
 class _AddPostScreenState extends State<AddPostScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-    child: IconButton(icon: const Icon(Icons.upload),onPressed: () {
-      
-    },),
+    // return Center(
+    //   child: IconButton(
+    //     icon: const Icon(Icons.upload),
+    //     onPressed: () {},
+    //   ),
+    // );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: const Text('Post to'),
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Post',
+                style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              ))
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 25,
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1695137300649-57882b305d34?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80'),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.35,
+                child: const TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Write a caption...', border: InputBorder.none),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+                width: 45,
+                child: AspectRatio(
+                  aspectRatio: 487 / 451,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              'https://images.unsplash.com/photo-1695137300649-57882b305d34?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80',
+                            ),
+                            fit: BoxFit.fill,
+                            alignment: FractionalOffset.topCenter)),
+                  ),
+                ),
+              ),
+              const Divider(),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
